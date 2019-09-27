7d9a6d15c8ec8353e0c7c660099ed92e1a17e849
cYa9-inu <cYa9forgithub@gmail.com>
2019-09-27 17:44:15 +0900

    前後の移動可能に

--- Assets/Scripts/ButtonA_B.cs
+++ Assets/Scripts/ButtonA_B.cs
@@ -3,14 +3,59 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ButtonA_B : MonoBehaviour {
+    [SerializeField]
+    public GameObject doggy;
+
+    //Aボタン
+    //押しっぱなし判定の間隔
+    public float intervalAction = 0.02f;
+
+    //次の押下判定時間
+    float nextTimeA = 0f;
+    float nextTimeB = 0f;
+
+    //押下状態
+    public bool pressedA;
+    public bool pressedB;
+
 	public void OnAbuttonClick()
 	{
         Debug.Log("Abutton Click");
+
+        Vector3 pos = doggy.transform.position;
+        pos.x += 0.1f;
+        doggy.transform.position = pos;
 	}
 
+    public void OnAbuttonDown()
+    {
+        pressedA = true;
+        nextTimeA = Time.realtimeSinceStartup + intervalAction;
+    }
+
+    public void OnAbuttonUp()
+    {
+        pressedA = false;
+    }
+
+
+    //Bボタン
     public void OnBbuttonClick()
     {
-        Debug.Log("BbuttonClic");
+        Debug.Log("BbuttonClick");
+        Vector3 pos = doggy.transform.position;
+        pos.x -= 0.1f;
+        doggy.transform.position = pos;
+    }
+
+    public void OnBbuttonDown()
+    {
+        pressedB = true;
+        nextTimeB = Time.realtimeSinceStartup + intervalAction;
+    }
+    public void OnBbuttonUp()
+    {
+        pressedB = false;
     }
 
 	// Use this for initialization
@@ -20,6 +65,16 @@ public class ButtonA_B : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        if(pressedA && Time.realtimeSinceStartup < nextTimeA)
+        {
+            nextTimeA = Time.realtimeSinceStartup + intervalAction;
+            OnAbuttonClick();
+        }
 		
+        if (pressedB && Time.realtimeSinceStartup < nextTimeB)
+        {
+            nextTimeB = Time.realtimeSinceStartup + intervalAction;
+            OnBbuttonClick();
+        }
 	}
 }
