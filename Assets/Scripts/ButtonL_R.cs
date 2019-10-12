213aa7e36c94b718b51995daff62390d1e61db01
cYa9-inu <cYa9forgithub@gmail.com>
2019-10-12 15:03:30 +0900

    前後左右

--- Assets/Scripts/ButtonL_R.cs
+++ Assets/Scripts/ButtonL_R.cs
@@ -6,25 +6,52 @@ public class ButtonL_R : MonoBehaviour {
     [SerializeField]
     public GameObject doggy;
 
-    //Aボタン
     //押しっぱなし判定の間隔
     public float intervalAction = 0.02f;
 
     //次の押下判定時間
     float nextTimeL = 0f;
     float nextTimeR = 0f;
+    float nextTimeF = 0f;
+    float nextTimeBk = 0f;
 
     //押下状態
     public bool pressedL;
     public bool pressedR;
+    public bool pressedF;
+    public bool pressedBk;
 
-	public void OnLbuttonClick()
+    //前進ボタン
+    public void OnFbuttonClick()
+    {
+        Debug.Log("Fbutton Click");
+
+        Transform tf = doggy.transform;
+        tf.Translate(Vector3.forward * 0.1f);
+
+    }
+
+    public void OnFbuttonDown()
+    {
+        pressedF = true;
+        nextTimeF = Time.realtimeSinceStartup + intervalAction;
+    }
+
+    public void OnFbuttonUp()
+    {
+        pressedF = false;
+    }
+
+
+
+    //Lボタン
+    public void OnLbuttonClick()
 	{
         Debug.Log("Lbutton Click");
 
-        Vector3 pos = doggy.transform.position;
-        pos.x += 0.1f;
-        doggy.transform.position = pos;
+        Transform tf = doggy.transform;
+        tf.Translate(Vector3.left*0.1f);
+        
 	}
 
     public void OnLbuttonDown()
@@ -39,13 +66,12 @@ public class ButtonL_R : MonoBehaviour {
     }
 
 
-    //Bボタン
+    //Rボタン
     public void OnRbuttonClick()
     {
         Debug.Log("RbuttonClick");
-        Vector3 pos = doggy.transform.position;
-        pos.x -= 0.1f;
-        doggy.transform.position = pos;
+        Transform tf = doggy.transform;
+        tf.Translate(Vector3.right * 0.1f);
     }
 
     public void OnRbuttonDown()
@@ -58,14 +84,40 @@ public class ButtonL_R : MonoBehaviour {
         pressedR = false;
     }
 
-	// Use this for initialization
-	void Start () {
+    //後進ボタン
+    public void OnBkbuttonClick()
+    {
+        Debug.Log("BkbuttonClick");
+        Transform tf = doggy.transform;
+        tf.Translate(Vector3.back * 0.1f);
+    }
+
+    public void OnBkbuttonDown()
+    {
+        pressedBk = true;
+        nextTimeBk = Time.realtimeSinceStartup + intervalAction;
+    }
+    public void OnBkbuttonUp()
+    {
+        pressedBk = false;
+    }
+
+
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if(pressedL && Time.realtimeSinceStartup < nextTimeL)
+        if (pressedF && Time.realtimeSinceStartup < nextTimeF)
+        {
+            nextTimeF = Time.realtimeSinceStartup + intervalAction;
+            OnFbuttonClick();
+        }
+
+        if (pressedL && Time.realtimeSinceStartup < nextTimeL)
         {
             nextTimeL = Time.realtimeSinceStartup + intervalAction;
             OnLbuttonClick();
@@ -76,5 +128,11 @@ public class ButtonL_R : MonoBehaviour {
             nextTimeR = Time.realtimeSinceStartup + intervalAction;
             OnRbuttonClick();
         }
-	}
+
+        if (pressedBk && Time.realtimeSinceStartup < nextTimeBk)
+        {
+            nextTimeBk = Time.realtimeSinceStartup + intervalAction;
+            OnBkbuttonClick();
+        }
+    }
 }
